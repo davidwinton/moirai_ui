98d61c667284f40a6aed47709fd96d9a24975ee6
David Winton <david.a.winton@gmail.com>
2025-01-15 17:28:11 -0500

    Added DefiLlama api for some assets

---
+++ app/api/defillama/[id]/route.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { NextResponse } from 'next/server';
+
+const DEFI_LLAMA_API_URL = 'https://api.llama.fi/updatedProtocol/';
+
+/**
+ * Fetch data from the DefiLlama API.
+ * @param {object} params - Optional query parameters for the API call.
+ * @returns {Promise<any>} - The API response data.
+ */
+
+export async function GET ( request: NextFetchRequestConfig, { params }: { params: { id: string }}) {
+  const { id } = params;
+  try {
+    const response = await axios.get(DEFI_LLAMA_API_URL + id, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      params,
+    });
+    return NextResponse.json(response.data); 
+  } catch (error: any) {
+    console.error('Error fetching Harmonic data:', error.response?.data || error.message);
+    throw new Error(error.response?.data?.message || 'Failed to fetch data');
+  }
+};
