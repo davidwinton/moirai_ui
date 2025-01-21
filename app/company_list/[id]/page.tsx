c4a120a22e98edf9ba368cc245874e4a9e5e218c
David Winton <davidwinton@Davids-MBP.fios-router.home>
2025-01-21 10:35:35 -0500

    Cleaned up in-flight scoring

--- app/company_list/[id]/page.tsx
+++ app/company_list/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { DefaultPageLayout } from "@/subframe/layouts/DefaultPageLayout";
 import CompanyDetails from "components/CompanyDetail/CompanyDetail";
 
 // Define the list page
@@ -5,6 +6,7 @@ const IdListPage: React.FC = () => {
     const recs = [56556915,53938838,18656035, 43276461,10195420,55016033,47467530,56362167,23491747,7584821,21776594,4211342,10909956,47596274,11148643,4160964,53068584,55883677,44597548,54813562,57544641,37066783,45206273,42851860,4116225,12760858,56739311,57028703,53927583,41922392,3081893,56244584,12587815,31723608,19390514,38808915,55218902,3863172,56731501,55560540]
   
     return (
+        <DefaultPageLayout>
       <div className="container p-6">
         <h1 className="text-2xl font-bold mb-4">Recommended Companies</h1>
         <div className="flex flex-col"> 
@@ -13,6 +15,7 @@ const IdListPage: React.FC = () => {
           ))}
         </div>
       </div>
+      </DefaultPageLayout>
     );
   };
   
